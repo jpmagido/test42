0356e51d8adaaeefa8e0c83b2a3101cc02e6244d
jpmagido <jpmagido@me.com>
2020-05-22 20:51:25 +0200

    createa seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def perform
+  destroy
+  create_users
+  create_user_test
+end
+
+def destroy
+  Article.destroy_all
+  User.destroy_all
+end
+
+def create_users
+  100.times do
+    FactoryBot.create(:user)
+  end
+  p '100 users created ✅'
+end
+
+def create_user_test
+  User.new(email: 'test@test.com', password: 'blablabla')
+  p "User created; password: 'blablabla', email: 'test@test.com' ✅"
+end
+
+def create_articles
+  100.times do
+    FactoryBot.create(:article)
+  end
+  p '100 articles created ✅'
+end
+
+perform
